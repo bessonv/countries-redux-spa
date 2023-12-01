1a04aec6d7beff02d6613901760892254fb123d5
bessonv <bessonov94@gmail.com>
2023-12-01 16:14:35 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { Main } from './components/Main';
+import { Header } from './components/Header';
+import { HomePage } from './pages/HomePage';
+import { Details } from './pages/Details';
+import { NotFound } from './pages/NotFound';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/country/:name" element={<Details />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Main>
+    </>
+  )
+}
+
+export default App;
