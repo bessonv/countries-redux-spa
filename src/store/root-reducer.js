90c84dbf7397fb510c34ff1713e9593aa992da9e
bessonv <bessonov94@gmail.com>
2023-12-01 17:22:41 +0300

    add countries store state and request

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { themeReducer } from "./theme/theme-reducer";
+import { countiresReducer } from "./countries/countries-reducer";
 
 export const rootReducer = combineReducers({
   theme: themeReducer,
+  countries: countiresReducer,
 });
